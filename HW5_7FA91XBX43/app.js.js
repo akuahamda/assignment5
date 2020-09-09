d56509fb5d3379d5d11b0b1aafdec3de569e2728
akuahamda <68547502+akuahamda@users.noreply.github.com>
2020-09-10 02:28:25 +0800

    Add files via upload

---
+++ HW5_7FA91XBX43/app.js.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost/user", { useNewUrlParser: true });
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public static folder
+app.use(express.static("public"));
+
+// Routes
+app.get("/", function(req, res) {
+  res.send("Hello from demo app!");
+});
+
+// Route for creating a new Product
+app.post("/product", function(req, res) {
+  console.log("in post request");
+  Product.create(req.body)
+    .then(function(data) {
+      // If we were able to successfully create a Product, send it back to the client
+      res.json(data);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("Listening on port " + PORT + ".");
+});
